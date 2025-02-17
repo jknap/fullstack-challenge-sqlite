5fc6378cbe5b14b29ede1be1ae0bad9242ac0b9f
jknap <jsknap@hotmail.com>
2025-02-17 13:58:50 +0100

    add a comment for the database choice

--- prisma/prismaClient.ts
+++ prisma/prismaClient.ts
@@ -1,3 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
+// TODO (Database): Use a standard relational database like PostgreSQL
+// Postgres is more robust and has better support for complex queries, transactions, and concurrency
+// SQLite is more suitable for small projects or simple applications
 export const prismaClient = new PrismaClient();
