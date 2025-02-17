ec8fe8ea4962e4192d64f84a4079002362a2c541
jknap <jsknap@hotmail.com>
2025-02-17 14:18:29 +0100

    add a comment about SSG and ISR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,12 @@ import { trpcReact } from "@/trpc/trpcReact";
 import { Typography } from "@mui/material";
 import { PostsList } from "./components/PostsList";
 
+// TODO (SSG): Implement static generation (SSG) for initial page load and SEO optimization
+// Use getStaticProps to fetch data at build time
+// That strategy might be actually more important for the specific /posts/:postId page
+// Another options is to implement incremental static regeneration (ISR)
+// See https://nextjs.org/docs/pages/building-your-application/data-fetching/incremental-static-regeneration
+
 export default function Home() {
   // TODO (Performance): Add pagination to improve performance (limit and offset)
   // Can also use useInfiniteQuery from trpc
